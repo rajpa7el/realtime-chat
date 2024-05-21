ae2f808fd7a7a865653ebc3be1aa13374de4d9ac
rajpa7el <rajpatel112003@gmail.com>
2024-05-21 09:21:29 -0230

    Uploaded

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js';
+
+const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+
+        if (!token){
+            return res.status(401).json({error: "No Token Provided"});            
+        } 
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decoded){
+            return res.status(401).json({error: "Invalid Provided"}); 
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+        
+        if(!user){
+            return res.status(401).json({error: "User not found"});
+        }
+
+        req.user=user;
+        next();
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: error.message || "Internal Server Error"});
+    }
+}
+
+export default protectRoute;
---
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -0,0 +1,50 @@
+import { IoSearchSharp } from "react-icons/io5";
+import { useState } from "react";
+import useConversation from "../../store/useConversation";
+import useGetConversation from "../../hooks/useGetConversations";
+import toast from 'react-hot-toast';
+
+const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversation();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+
+    if (!conversations) {
+      console.error('Conversations are undefined');
+      toast.error('Unable to fetch conversations.');
+      return;
+    }
+
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch('');
+    } else {
+      toast.error('No User Found');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='flex items-center gap-2'>
+      <input
+        type='text'
+        placeholder='Search…'
+        className='input input-bordered rounded-full'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
+        <IoSearchSharp className='w-6 h-6 outline-none' />
+      </button>
+    </form>
+  );
+};
+
+export default SearchInput;
